9da4f7b9b507a1df85124132a308edb3bfe96349
Raed-Alqabas <rayeddd55@gmail.com>
2025-08-27 15:30:14 +0000

    hbnb

---
+++ part2/app/api/v1/__init__.py
@@ -0,0 +1,8 @@
+from .ping import ns as health_ns
+from .users import ns as users_ns            
+from .amenities import ns as amenities_ns    
+
+def register_namespaces(api):
+    api.add_namespace(health_ns, path="/health")
+    api.add_namespace(users_ns, path="/users")          
+    api.add_namespace(amenities_ns, path="/amenities")
