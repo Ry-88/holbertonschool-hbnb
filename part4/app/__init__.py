7946e618ddf8987a34b57afed1f92b4e8ae09184
moha88z <mohammed.alayda88@gamil.com>
2025-10-06 16:43:44 +0300

    part4

--- part4/app/__init__.py
+++ part4/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.extensions import bcrypt
+from app.api.v1.auth import api as auth_ns
 import config
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
@@ -24,7 +25,8 @@ def create_app(config_class=config.DevelopmentConfig):
     # from app.models import db
     db.init_app(app)
 
-    # Register the users namespace
+    # Register namespaces
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
